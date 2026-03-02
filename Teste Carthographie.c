e9ce934391b23b184398eea73a91ec326139911b
TimTheGuyDrifitingOnTheRunway <157991876+TimTheGuyDrifitingOnTheRunway@users.noreply.github.com>
2026-03-02 21:19:10 +0100

    nice one

--- Teste Carthographie.c
+++ Teste Carthographie.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-
+// what if i wanted to have fun
 /*****************************************CONSTANTES**************************************************/
 #define SIZE 11
 #define PIECESIZE 3
