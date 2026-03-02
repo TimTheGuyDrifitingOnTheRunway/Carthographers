0bbf613b121257258cd7849a02efae24cf31d1c9
TimTheGuyDrifitingOnTheRunway <157991876+TimTheGuyDrifitingOnTheRunway@users.noreply.github.com>
2026-03-02 21:14:44 +0100

    coin

--- Teste Carthographie.c
+++ Teste Carthographie.c
@@ -67,7 +67,7 @@ int drawU(FeuilleCarte f, Position pos, int rotation, int material);//DEPRECIé
 int drawShape(FeuilleCarte f, Piece piec, Position pos, int rotation, int material);//fonction Universelle
 
 
-//focacie   
+ 
 
 //placement des formes/////////////////////////////////////////////////
 int placementU(FeuilleCarte f, int material);//DEPRECIée
