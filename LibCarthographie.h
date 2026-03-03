cfb7e04dde4d1e1957787a911930878d26acfd1b
Mathéo bouvet <Mathplaysbouvet@gmail.com>
2026-03-03 20:05:00 +0100

    Passage des Voisins de 8 à 4 dans getVoisinMaterialPos, comme dans les règles, page 9, et qlqs autres modifs inutiles comme enlever des lignes vides inutiles

--- LibCarthographie.h
+++ LibCarthographie.h
@@ -32,8 +32,6 @@
 #define ACTUAL 12
 #define NEXT 13
 
-
-
 /*****************************************TYPEDEFS**************************************************/
 typedef int FeuilleCarte[SIZE][SIZE];
 typedef int Piece[PIECESIZE][PIECESIZE];
