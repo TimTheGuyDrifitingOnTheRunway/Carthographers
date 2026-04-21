619f6b0c94c13f52b1b7425977f604ddcdf91314
Mathéo bouvet <Mathplaysbouvet@gmail.com>
2026-04-21 22:36:26 +0200

    Changement des toutes les polices d'écritures, réglage de l'apparence, et ajout de Constantes pour plus de visibilité. Ajout des images des saisons

--- UI.h
+++ UI.h
@@ -10,15 +10,16 @@ ScreenID RunRules(GameState* gs);
 ScreenID RunKeybinds(GameState* gs);
 
 void DrawBackgroudMenu(Rectangle r);
-void DrawTitleEx(Rectangle r);
-int DrawTitle();
+//void DrawTitleEx(GameState* gs, Rectangle r);
+int DrawTitle(GameState* gs);
 void SortRectangles(Rectangle** rlist, int listLen, int pad, float anchorPoint);		// Trie les rectangles de manière à les rendre alignés sur le 1er élément, en fonction du point d'encrage (0 : en colonne à partir de la gauche, .5f : milieu, 1 : droite)
 
-Vector2 MeasureTextWrapped(const char* text, Rectangle r, int fs, Color textColor);
+Vector2 MeasureTextWrapped(const char* text, Rectangle r, int fs);
+Vector2 MeasureTextWrappedEx(Font font, const char* text, Rectangle r, int fs);
 Vector2 DrawTextWrapped(const char* text, Rectangle r, int fs, Color textColor);
+Vector2 DrawTextWrappedEx(Font font, const char* text, Rectangle r, int fs, int stroke, Color textColor, Color strokeColor);
 
 void DrawButton(Button* btn);
-void DrawButtonEx(Button* btn, Font font);
 
 void ImageRoundCorner(Image* image, float radius);
 void ImageRoundedCorner(Image* image, float roundness);
@@ -29,8 +30,8 @@ bool IsCharAllowed(char c);
 void DrawFullInputBoxEx(InputBox* box, int stroke, Color bkgColor, Color strokeColor);
 
 
-void DrawStrokeTextEx(const char* text, int x, int y, int fontSize, Color textColor, Color strokeColor, int thickness);
-void DrawStrokeText(const char* text, int x, int y, int fontSize, Color textColor, Color strokeColor);
+void DrawStrokeTextEx(Font font, const char* text, int x, int y, int fontSize, int spacing, Color textColor, Color strokeColor, int thickness);
+void DrawStrokeText(Font font, const char* text, int x, int y, int fontSize, Color textColor, Color strokeColor);
 void DrawRectangleRoundedStrokeEx(Rectangle rec, float roundness, int segments, float lineThick, Color rectColor, Color strokeColor);
 void DrawRectangleRoundedStroke(Rectangle rec, float roundness, int segments, Color rectColor, Color strokeColor);
 void DrawRectangleStroke(Rectangle rec, float lineThick, Color rectColor, Color strokeColor);
